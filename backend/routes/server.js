f0c296a8b157f7bd03de17affbd46aba33535b20
John Doe <johndoe@example.com>
2024-11-25 15:25:15 +0700

    123213

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -7,6 +7,14 @@ import cors from 'cors';
 // Initialize the Express app
 const app = express();
 
+app.use(express.json())
+
+const mockUsers = [
+    {id: 1, username: "Anson", displayName: "Anson"},
+    {id: 2, username: "Jack", displayName: "Jack"},
+    {id: 3, username: "Duy", displayName: "Duy"}
+]
+
 // Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
@@ -28,15 +36,24 @@ app.get("/", (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-    res.send([
-        {id: 1, username: "Anson", displayName: "Anson"},
-        {id: 2, username: "Jack", displayName: "Jack"},
-        {id: 3, username: "Duy", displayName: "Duy"}
-    ])
+    const {query: {filter, value}} = req;
+    if (!filter && !value) return res.send(mockUsers)
+    if (filter && value) return res.send(mockUsers.filter((user) => user[filter] === value))
 })
 
 app.get('/api/users/:id', (req, res) => {
+    console.log(req.params)
+    const parseId = parseInt(req.params.id)
+    console.log(parseId)
+    if (isNaN(parseId)) return res.status(400).send({msg: "Bad Request. Invalid Id"}) //400 = bad request
+    const findUser = mockUsers.find((user) => user.id === parseId)
+    if (!findUser) return res.sendStatus(404)
+    return res.send(findUser)
+})
 
+app.post('/api/users', (req, res) => {
+    console.log(req.body)
+    return res.sendStatus(200)
 })
 
 app.get('/api/products', (req, res) => {
@@ -47,6 +64,8 @@ app.get('/api/products', (req, res) => {
         {id: 123, name:'Chicken breast', price:'12,99'},
     ])
 })
+
+app.get
 
 // Start the server
 const PORT = 3000;
