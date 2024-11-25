23d3eb629a814c0a5ee2c103f446272165d685fb
John Doe <johndoe@example.com>
2024-11-26 00:30:52 +0700

    123213

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -9,18 +9,45 @@ const app = express();
 
 app.use(express.json())
 
+
+
 const mockUsers = [
     {id: 1, username: "Anson", displayName: "Anson"},
     {id: 2, username: "Jack", displayName: "Jack"},
-    {id: 3, username: "Duy", displayName: "Duy"}
+    {id: 3, username: "Duy", displayName: "Duy"},
+    {id: 4, username: "Mia", displayName: "Mia"},
+    {id: 5, username: "Liam", displayName: "Liam"},
+    {id: 6, username: "Emma", displayName: "Emma"},
+    {id: 7, username: "Noah", displayName: "Noah"},
+    {id: 8, username: "Sophia", displayName: "Sophia"},
+    {id: 9, username: "Olivia", displayName: "Olivia"},
+    {id: 10, username: "Lucas", displayName: "Lucas"},
+    {id: 11, username: "Ethan", displayName: "Ethan"},
+    {id: 12, username: "Ava", displayName: "Ava"},
+    {id: 13, username: "Harper", displayName: "Harper"}
 ]
 
 // Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
-
 // Database connection configuration
 
+const loggingMiddleWare = (req, res, next) => {
+    console.log(`${req.method} - ${req.url}`)
+    next()
+}
+
+const resolveIndexByUserId = (req, res, next) => {
+    const {
+        body, params: {id}
+    } = req
+    const parsedId = parseInt(id)
+    if (isNaN(parsedId)) return res.sendStatus(400)
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId)
+    if (findUserIndex === -1) return res.sendStatus(404)
+    req.findUserIndex = findUserIndex
+    next()
+}
 
 // Connect to the MySQL database
 db.connect((err) => {
@@ -31,7 +58,10 @@ db.connect((err) => {
     console.log('Connected to MySQL database.');
 });
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
+    console.log("Base URL")
+    next()
+}, (req, res) => {
     res.send("Hello, love to see you")
 });
 
@@ -41,19 +71,17 @@ app.get('/api/users', (req, res) => {
     if (filter && value) return res.send(mockUsers.filter((user) => user[filter] === value))
 })
 
-app.get('/api/users/:id', (req, res) => {
-    console.log(req.params)
-    const parseId = parseInt(req.params.id)
-    console.log(parseId)
-    if (isNaN(parseId)) return res.status(400).send({msg: "Bad Request. Invalid Id"}) //400 = bad request
-    const findUser = mockUsers.find((user) => user.id === parseId)
-    if (!findUser) return res.sendStatus(404)
-    return res.send(findUser)
+app.get('/api/users/:id', resolveIndexByUserId, loggingMiddleWare, (req, res) => {
+    const {findUserIndex} = req
+    return res.send(mockUsers[findUserIndex])
 })
 
 app.post('/api/users', (req, res) => {
-    console.log(req.body)
-    return res.sendStatus(200)
+    const {body} = req
+    console.log(body)
+    const newUser = {id: mockUsers[mockUsers.length - 1].id+1, ...body}
+    mockUsers.push(newUser)
+    return res.status(201).send(newUser)
 })
 
 app.get('/api/products', (req, res) => {
@@ -65,7 +93,23 @@ app.get('/api/products', (req, res) => {
     ])
 })
 
-app.get
+app.put('/api/users/:id', resolveIndexByUserId, loggingMiddleWare, (req, res) => {
+    const {body, findUserIndex} = req
+    mockUsers[findUserIndex] = {findUserIndex, ...body}
+    return res.sendStatus(200)
+})
+
+app.patch('/api/users/:id', resolveIndexByUserId, loggingMiddleWare,(req, res) => {
+    const {body, findUserIndex} = req
+    mockUsers[findUserIndex] = {...mockUsers[findUserIndex], ...body}
+    return res.sendStatus(200)
+})
+
+app.delete("/api/users/:id", resolveIndexByUserId, loggingMiddleWare,(req, res) => {
+    const {findUserIndex} = req
+    mockUsers.splice(findUserIndex, 1)
+    return res.sendStatus(200)
+})
 
 // Start the server
 const PORT = 3000;
