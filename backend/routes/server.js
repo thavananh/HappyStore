c28716459365baec6a8ac7c48f54364b567b9301
duycute <thavananh@gmail.com>
2024-11-28 02:47:58 +0700

    123213

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import { mockUsers } from './constant.js'
 import passport from 'passport'
-
+import "../strategies/local-stategies.js"
 
 // Initialize the Express app
 const app = express()
@@ -29,6 +29,11 @@ app.use(
 )
 app.use(cors())
 app.use(bodyParser.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 
 app.use(userRouter)
 app.use(productRouter)
@@ -53,42 +58,45 @@ app.get('/', (req, res) => {
     res.send('Hello, love to see you')
 })
 
-app.post('/api/auth', (req, res) => {
-    const {body:{username, password}} = req
-    const findUser = mockUsers.find(user => user.username === username)
-    if (!findUser || findUser.password !== password) {
-        return res.status(401).send('BAD CREDENTIALS')
-    }
-    req.session.user = findUser
-    return res.status(200).send(findUser)
-})
+// app.post('/api/auth', (req, res) => {
+//     const {body:{username, password}} = req
+//     const findUser = mockUsers.find(user => user.username === username)
+//     if (!findUser || findUser.password !== password) {
+//         return res.status(401).send('BAD CREDENTIALS')
+//     }
+//     req.session.user = findUser
+//     return res.status(200).send(findUser)
+// })
+//
+// app.get('/api/auth/status', (req, res) => {
+//     console.log(req.session)
+//     return req.session.user ? res.status(200).send(req.session.user) : res.status(404).send({msg: "Not Authorized"})
+// })
+//
+// app.post('/api/auth/cart', (req, res) => {
+//     if (!req.session.user) return res.status(401).send('BAD CREDENTIALS')
+//     console.log(req.session)
+//     const {body: item} = req
+//     const {cart} = req.session
+//     console.log(cart)
+//     if (cart) {
+//         cart.push(item)
+//     }
+//     else {
+//         req.session.cart = [item]
+//     }
+//     return res.status(201).send(item)
+// })
+//
+// app.get('/api/auth/cart', (req, res) => {
+//     if (!req.session.user) return res.status(401).send('BAD CREDENTIALS');
+//     if (req.session.cart) {return res.status(200).send(req.session.cart)}
+//     else {return res.status(200).send([])}
+// })
 
-app.get('/api/auth/status', (req, res) => {
-    console.log(req.session)
-    return req.session.user ? res.status(200).send(req.session.user) : res.status(404).send({msg: "Not Authorized"})
-})
+app.post('/api/auth', passport.authenticate('local'), (req, res) => {
 
-app.post('/api/auth/cart', (req, res) => {
-    if (!req.session.user) return res.status(401).send('BAD CREDENTIALS')
-    console.log(req.session)
-    const {body: item} = req
-    const {cart} = req.session
-    console.log(cart)
-    if (cart) {
-        cart.push(item)
-    }
-    else {
-        req.session.cart = [item]
-    }
-    return res.status(201).send(item)
 })
-
-app.get('/api/auth/cart', (req, res) => {
-    if (!req.session.user) return res.status(401).send('BAD CREDENTIALS');
-    if (req.session.cart) {return res.status(200).send(req.session.cart)}
-    else {return res.status(200).send([])}
-})
-
 
 // Start the server
 const PORT = 3000
--- backend/strategies/local-stategies.js
+++ backend/strategies/local-stategies.js
@@ -1,2 +1,29 @@
+import passport from 'passport'
 
-app
+import {Strategy} from 'passport-local'
+import { mockUsers } from '../routes/constant.js'
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+export default passport.use(
+    new Strategy((username, password, done) => {
+        try {
+            console.log(`username: ${username}`)
+            console.log(`password: ${password}`)
+            const findUser = mockUsers.find((user) => user.username === username)
+            if (!findUser) {
+                throw new Error('User not found')
+            }
+            if (findUser.password !== password) {
+                throw new Error('Invalid Credentials')
+            }
+            done(null, findUser)
+        }
+        catch (err) {
+            done(err, null)
+        }
+
+    })
+)
